cc7829a5ca7dc3102200aab6b2beb4e16dae58c3
zhanghailiang <zhang.zhanghailiang@huawei.com>
2015-02-10 15:08:11 +0000

    COLO: Disable qdev hotplug when VM is in COLO mode
    COLO do not support qdev hotplug migration, disable it.
    
    Signed-off-by: zhanghailiang <zhang.zhanghailiang@huawei.com>
    Signed-off-by: Yang Hongyang <yanghy@cn.fujitsu.com>

--- migration/colo.c
+++ migration/colo.c
@@ -10,6 +10,7 @@
  * later.  See the COPYING file in the top-level directory.
  */
 
+#include "hw/qdev-core.h"
 #include "qemu/timer.h"
 #include "sysemu/sysemu.h"
 #include "migration/colo.h"
@@ -348,6 +349,7 @@ out:
 static void *colo_thread(void *opaque)
 {
     MigrationState *s = opaque;
+    int dev_hotplug = qdev_hotplug;
     QEMUFile *colo_control = NULL;
     int64_t current_time, checkpoint_time = qemu_clock_get_ms(QEMU_CLOCK_HOST);
     int i, ret;
@@ -362,6 +364,8 @@ static void *colo_thread(void *opaque)
         error_report("Open colo_control failed!");
         goto out;
     }
+
+    qdev_hotplug = 0;
 
     /*
      * Wait for Secondary finish loading vm states and enter COLO
@@ -459,6 +463,8 @@ out:
     qemu_bh_schedule(s->cleanup_bh);
     qemu_mutex_unlock_iothread();
 
+    qdev_hotplug = dev_hotplug;
+
     return NULL;
 }
 
@@ -516,9 +522,12 @@ void *colo_process_incoming_checkpoints(void *opaque)
     struct colo_incoming *colo_in = opaque;
     QEMUFile *f = colo_in->file;
     int fd = qemu_get_fd(f);
+    int dev_hotplug = qdev_hotplug;
     QEMUFile *ctl = NULL, *fb = NULL;
     int i, ret;
     uint64_t total_size;
+
+    qdev_hotplug = 0;
 
     colo = qemu_coroutine_self();
     assert(colo != NULL);
@@ -700,6 +709,8 @@ out:
     qsb_free(colo_buffer);
 
     loadvm_exit_colo();
+
+    qdev_hotplug = dev_hotplug;
 
     return NULL;
 }
