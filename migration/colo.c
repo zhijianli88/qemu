cdcaae5ae23afbd5ab3352eb688afe1ea45b6005
zhanghailiang <zhang.zhanghailiang@huawei.com>
2015-03-10 11:10:51 +0000

    COLO: Add colo-set-checkpoint-period command
    With this command, we can control the period of checkpoint, if
    there is no comparison of net packets.
    
    Signed-off-by: zhanghailiang <zhang.zhanghailiang@huawei.com>
    Signed-off-by: Li Zhijian <lizhijian@cn.fujitsu.com>

--- migration/colo.c
+++ migration/colo.c
@@ -17,6 +17,7 @@
 #include "qemu/error-report.h"
 #include "migration/migration-failover.h"
 #include "net/colo-nic.h"
+#include "qmp-commands.h"
 
 /*
 * We should not do checkpoint one after another without any time interval,
@@ -70,6 +71,9 @@ enum {
 static QEMUBH *colo_bh;
 static bool vmstate_loading;
 static Coroutine *colo;
+
+int64_t colo_checkpoint_period = CHECKPOINT_MAX_PEROID;
+
 /* colo buffer */
 #define COLO_BUFFER_BASE_SIZE (4 * 1024 * 1024)
 QEMUSizedBuffer *colo_buffer;
@@ -83,6 +87,11 @@ bool migrate_in_colo_state(void)
 {
     MigrationState *s = migrate_get_current();
     return (s->state == MIGRATION_STATUS_COLO);
+}
+
+void qmp_colo_set_checkpoint_period(int64_t value, Error **errp)
+{
+    colo_checkpoint_period = value;
 }
 
 static bool colo_runstate_is_stopped(void)
@@ -361,7 +370,7 @@ static void *colo_thread(void *opaque)
          * and then check if we need checkpoint again.
          */
         current_time = qemu_clock_get_ms(QEMU_CLOCK_HOST);
-        if (current_time - checkpoint_time < CHECKPOINT_MAX_PEROID) {
+        if (current_time - checkpoint_time < colo_checkpoint_period) {
             g_usleep(100000);
             continue;
         }
